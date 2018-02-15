f6dee2b44e08904162bef7a262910ee8c5cdd1dc
xwkuang5 <louisconfussing@gmail.com>
2018-02-14 19:20:56 -0500

    Create parse_physionet.py for parsing physionet data

--- parsing/parse_physionet.py
+++ parsing/parse_physionet.py
@@ -1,14 +1,36 @@
+import os
+import re
 import glob
 import operator
 import numpy as np
 import argparse
 
-from parse_wfdb_description import parse_wfdb_description
+def parse_wfdb_description(desc_filename):
+    """Parse the wfdb description files by finding the names of the channels used
+
+    Parameters:
+        desc_filename   - the input description file
+
+    Returns:
+        matches         - names of the channels
+    """
+
+    if os.path.exists(desc_filename):
+        try:
+            with open(desc_filename, "r") as f:
+                regex = r".*Description: (.*)$"
+                content = f.read()
+                matches = re.findall(regex, content, flags=re.MULTILINE)
+                return matches
+            f.close()
+        except:
+            # TODO: handle exception
+            raise
 
 def parse_edf(input_edf, channels=None):
     pass
 
-def get_channel_names_summary(input_dir):
+def summarize_channels(input_dir):
     """Get a summary about names of the channels used in the recordings in input_dir
 
     Parameters:
